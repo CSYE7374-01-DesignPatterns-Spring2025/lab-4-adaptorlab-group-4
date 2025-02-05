dbf5cee3b99e1d4cf5d0d9fc3f7e20be3afc37ca
ChaudharyAnshul <chaudhary.ans@northeastern.edu>
2025-02-04 20:05:53 -0500

    space removed

--- src/main/java/edu/neu/csye7374/AccumulatableAPI.java
+++ src/main/java/edu/neu/csye7374/AccumulatableAPI.java
@@ -2,7 +2,7 @@ package edu.neu.csye7374;
 import java.util.List;
 
 public interface AccumulatableAPI {
-   double accumulation(List<Double> itemPrices);
-   double payment(double payment);
+    double accumulation(List<Double> itemPrices);
+    double payment(double payment);
 }
 
